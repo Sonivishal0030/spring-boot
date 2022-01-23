2b737acb98d6c6648d9e55e6c064ab6e21ccde01
vishalmundawra@gmail.com <vishalmundawra@gmail.com>
2022-01-23 17:48:00 +0530

    added entity, service and repository layer

--- src/main/java/com/learn/domain/User.java
+++ src/main/java/com/learn/domain/User.java
@@ -2,10 +2,23 @@ package com.learn.domain;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
 @Data
 public class User {
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
+
+	private String name;
+
+	private String email;
+
+	private Date createdDate;
 
 }
